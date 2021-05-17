f3f9364275bade1cd03a7143e683a9eebd84fb62
varvarahurevich <gurevichvarvara@gmail.com>
2021-05-17 19:04:09 +0300

    Add requirements

--- plates/plates_detection/views.py
+++ plates/plates_detection/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render
 
 
 def load_plate(request):
+    if request.method == 'POST' and request.FILES['plate']:
+        print('ya')
+
     return render(request,
                   'plates_detection/load_plate.html')
